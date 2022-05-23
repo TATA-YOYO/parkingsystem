b64403b9efc2d995b75eda12a4a32d6cdba80c81
TATA-YOYO <4learnhacking@free.fr>
2022-05-23 10:31:25 +0200

    Basic Setup

--- src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
+++ src/main/java/com/parkit/parkingsystem/config/DataBaseConfig.java
@@ -13,7 +13,7 @@ public class DataBaseConfig {
         logger.info("Create DB connection");
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/prod","root","rootroot");
+                "jdbc:mysql://localhost:3306/prod?useSSL=false&serverTimezone=UTC","root","123456");
     }
 
     public void closeConnection(Connection con){
